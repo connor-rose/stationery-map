1cd7dfe14ac45ccf9fd707ae02717b501c1d2ef4
connor-rose <connor.rose@icloud.com>
2025-04-10 19:20:06 -0400

    Update index.js

--- worker/index.js
+++ worker/index.js
@@ -41,7 +41,7 @@ async function handleLocations() {
 }
 
 async function handleToken() {
-  const mapkitJWT = "eyJhbGciOiJFUzI1NiIsImtpZCI6Ijg5N01NOTRGTUciLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJBQjU2QlVOWkJDIiwiaWF0IjoxNzQ0MzIxMzgwLCJleHAiOjE3NDQ0MDc3ODAsImJpZCI6ImlDbG91ZC5jb20uYmlzY3VpdHN0dWRpb3MucGVuZWRleCJ9.bQmPj9Ic_nuy1B1nRznAMZyZCUYLOp8IZWvliRhHvfDWSVydl_dHkv85SD_HUShHiE5bvYrekNi8VDyfn_KW2A";
+  const mapkitJWT = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ijg5N01NOTRGTUcifQ.eyJpc3MiOiJBQjU2QlVOWkJDIiwiaWF0IjoxNzQ0MzI3MTc2LCJleHAiOjE3NDQ0MTM1NzYsImJpZCI6ImlDbG91ZC5jb20uYmlzY3VpdHN0dWRpb3MucGVuZWRleCJ9.gJg_hCZpZzD0IbgpKN94fy2SperWjc7MSbZvr-C-mC5v0rHQGwn3oFQEn0v25Q2Gn5uJQB5SFRfiZdUckW_qag";
 
   return new Response(mapkitJWT, {
 	headers: {
