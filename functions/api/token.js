246cfe85105b5c70a77ff74ab61fee9b9aa8c384
Connor Rose <connor.rose@icloud.com>
2025-05-01 00:42:56 -0400

    Move project to root directory and clean up old files

--- functions/api/token.js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  return new Response("eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ijg5N01NOTRGTUcifQ.eyJpc3MiOiJBQjU2QlVOWkJDIiwiaWF0IjoxNzQ0MzI3MTc2LCJleHAiOjE3NDQ0MTM1NzYsImJpZCI6ImlDbG91ZC5jb20uYmlzY3VpdHN0dWRpb3MucGVuZWRleCJ9.gJg_hCZpZzD0IbgpKN94fy2SperWjc7MSbZvr-C-mC5v0rHQGwn3oFQEn0v25Q2Gn5uJQB5SFRfiZdUckW_qag", {
-    status: 200,
-    headers: {
-      "Content-Type": "text/plain",
-      "Access-Control-Allow-Origin": "*"
-    }
-  });
-}
